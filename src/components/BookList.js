107c122f92c1271969313b61cf36ae67e314be94
Raessa Fathul Alim <raessa_pro@mpotech.net>
2019-06-18 01:44:29 +0800

    first learn day1

---
+++ src/components/BookList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+
+class BookList extends Component {
+
+    static contextType = ThemeContext;
+    render() { 
+        const { isLightTheme, light, dark } = this.context;
+
+        const theme = isLightTheme ? light : dark;
+
+        return (
+           <div className='book-list' style={{ color:theme.syntax, background:theme.bg}}>
+               <ul>
+                   <li style={{ background: theme.ui}}>the way you look</li>
+                   <li style={{ background: theme.ui}}>the name of wind</li>
+                   <li style={{ background: theme.ui}}>the final of love</li>
+               </ul>
+           </div>
+        );
+    }
+}
+ 
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+import { AuthContext } from '../context/AuthContext';
+
+class Navbar extends Component {
+    render() { 
+        return ( 
+
+            <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(themeContext) => {
+                    const { isLightTheme, light, dark } = themeContext;
+                    const theme = isLightTheme ? light : dark;
+                    const { isAuthenticated, toggleAuth} = authContext;
+                    return(
+                        <nav style={{background: theme.ui, color:theme.syntax}}>
+                            <h1>Context App</h1>
+                            <div>
+                                <button onClick={toggleAuth}>{isAuthenticated ? 'Logged Out' : 'Logged In'}</button>
+                            </div>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    )
+                }}</ThemeContext.Consumer>
+            )}</AuthContext.Consumer>
+            
+         );
+    }
+}
+ 
+export default Navbar;
